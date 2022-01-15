6aaa232ba6fe92e122f568684b96dabb9fab1240
Rakesh <rakeshabhinav65@gmail.com>
2022-01-15 23:44:02 +0530

    update js file

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 
-checkNumberButton.addEventListener('click', function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
+checkNumberButton.addEventListener('click', function getValues() {
+    // console.log(dateOfBirth.value, luckyNumber.value);
 })
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+}
+
+function calculateSum(dob) {
+    // dob= dob.replaceAll("-", "");
+    // let sum = 0;
+    // for
+    return 22;
+}
